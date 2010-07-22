4a137eaac8fc39f17edabfce87528ebd874c21b1
Wesley Mason <wes [at] 1stvamp.org>
2010-07-22 15:32:36 +0100

    Renamed batch file generated by windows_setup

--- windows_setup.bat
+++ windows_setup.bat
@@ -3,7 +3,7 @@ set PYTHONPATH=%PYTHONPATH%;C:\Python26\Lib\site-packages
 set PATH=%PATH%;C:\Python26;C:\Python26\Scripts
 python setup.py install
 
-> "%USERPROFILE%\Desktop\wtqt.bat" (
+> "%USERPROFILE%\Desktop\wtqt_cmd.bat" (
 @echo.@echo off
 @echo.set PYTHONPATH=%%PYTHONPATH%%;C:\Python26\Lib\site-packages
 @echo.set PATH=%%PATH%%;C:\Python26;C:\Python26\Scripts
